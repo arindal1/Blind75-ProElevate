ad903adab8a50126728153f45391ef7133be7517
Arindal Char <110285827+arindal1@users.noreply.github.com>
2024-06-04 11:16:11 +0530

    created 3.

---
+++ 3 - dynamic programming/3.LongestIncreasingSubsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ans;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(ans.empty()||ans.back()<nums[i])ans.push_back(nums[i]);
+            else
+            {
+                int in=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[in]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
+
+// O(nlogn)
